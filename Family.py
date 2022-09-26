67ea1cbb2a690d194933bd13b069f31950662245
ScienceWater <vtufo@sbcglobal.net>
2022-09-26 18:08:50 -0400

    Created Individual and Family classes.

---
+++ Family.py
@@ -0,0 +1,42 @@
+class Family():
+
+    def __init__(self, id):
+        self.__id = id
+        self.__marr = False
+        self.__husb = False
+        self.__wife = False
+        self.__children = []
+        self.__div = False
+    
+    def getID(self):
+        return self.__id
+
+    def setMarr(self, marr):
+        self.__marr = marr
+
+    def getMarr(self):
+        return self.__marr
+
+    def setHusb(self, husb):
+        self.__husb = husb
+    
+    def getHusb(self):
+        return self.__husb
+    
+    def setWife(self, wife):
+        self.__wife = wife
+    
+    def getWife(self):
+        return self.__wife
+
+    def addChild(self, child):
+        self.__children += [child]
+    
+    def getChildren(self):
+        return self.__children
+    
+    def setDiv(self, div):
+        self.__div = div
+    
+    def getDiv(self):
+        return self.__div
---
+++ Individual.py
@@ -0,0 +1,49 @@
+class Individual():
+
+    def __init__(self, id):
+        self.__id = id
+        self.__name = False
+        self.__sex = False
+        self.__birth = False
+        self.__death = False
+        self.__famC = False
+        self.__famS = []
+    
+    def getID(self):
+        return self.__id
+
+    def setName(self, name):
+        self.__name = name
+
+    def getName(self):
+        return self.__name
+
+    def setSex(self, sex):
+        self.__sex = sex
+    
+    def getSex(self):
+        return self.__sex
+    
+    def setBirth(self, birth):
+        self.__birth = birth
+    
+    def getBirth(self):
+        return self.__birth
+    
+    def setDeath(self, death):
+        self.__death = death
+    
+    def getDeath(self):
+        return self.__death
+    
+    def setFamC(self, famC):
+        self.__famC = famC
+    
+    def getFamC(self):
+        return self.__famC
+    
+    def addFamS(self, famS):
+        self.__famS += [famS]
+    
+    def getFamS(self):
+        return self.__famS
