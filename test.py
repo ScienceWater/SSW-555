f9f30cf8e4a9a567e2d489246865b5c3b5e0de83
ScienceWater <vtufo@sbcglobal.net>
2022-10-03 18:19:25 -0400

    Created test.py.

---
+++ test.py
@@ -0,0 +1,23 @@
+import unittest
+import GEDCOM_parse
+
+class TestSprint1(unittest.TestCase):
+
+    def story2(self):
+        '''Birth before marriage'''
+        pass
+
+    def story3(self):
+        '''Birth before death'''
+        pass
+
+    def story4(self):
+        '''Marriage before divorce'''
+        pass
+
+    def story5(self):
+        '''Marriage before death'''
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
