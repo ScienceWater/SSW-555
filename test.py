8de5c66579ea8b2bf5ba57af717d5f74d9c0d9e3
ScienceWater <vtufo@sbcglobal.net>
2022-10-03 21:45:19 -0400

    Finished sprint 1 test cases.

--- test.py
+++ test.py
@@ -8,11 +8,19 @@ class TestSprint1(unittest.TestCase):
 
     def testStory2(self):
         '''Birth before marriage'''
-        pass
+        self.assertFalse(birthBeforeMarriage(individuals["@I10@"])) # married before born (Agnes)
+        self.assertTrue(birthBeforeMarriage(individuals["@I6@"])) # married same day as born (Margo)
+        self.assertTrue(birthBeforeMarriage(individuals["@I1@"])) # married after born (Bob)
+        self.assertTrue(birthBeforeMarriage(individuals["@I9@"])) # never married (Otto)
+        self.assertTrue(birthBeforeMarriage(individuals["@I4@"])) # married twice after born (Stuart)
 
     def testStory3(self):
         '''Birth before death'''
-        pass
+        self.assertFalse(birthBeforeDeath(individuals["@I8@"])) # dies before born (Edith)
+        self.assertTrue(birthBeforeDeath(individuals["@I6@"])) # death date same as birth date (Margo)
+        self.assertTrue(birthBeforeDeath(individuals["@I10@"])) # dies after born (Agnes)
+        self.assertTrue(birthBeforeDeath(individuals["@I2@"])) # still alive, old (Gru)
+        self.assertTrue(birthBeforeDeath(individuals["@I1@"])) # still alive, young (Bob)
 
     def testStory4(self):
         '''Marriage before divorce'''
