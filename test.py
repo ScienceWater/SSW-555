6e8ba4a13ed99e340947da4d650a80141ba7145b
Andrew Quinlan <andrewquinlan22@gmail.com>
2022-10-03 21:20:21 -0400

    fixed test case function names

--- test.py
+++ test.py
@@ -6,15 +6,15 @@ class TestSprint1(unittest.TestCase):
     gedcom_file = open("error.ged")
     gedcom_parse(gedcom_file)
 
-    def story2(self):
+    def testStory2(self):
         '''Birth before marriage'''
         pass
 
-    def story3(self):
+    def testStory3(self):
         '''Birth before death'''
         pass
 
-    def story4(self):
+    def testStory4(self):
         '''Marriage before divorce'''
         self.assertFalse(marriageBeforeDivorce(families["@F1@"])) # marriage date after divorce date (Gru & Lucy)
         self.assertTrue(marriageBeforeDivorce(families["@F4@"])) # marriage date same as divorce date (Kevin & Margo)
@@ -22,7 +22,7 @@ class TestSprint1(unittest.TestCase):
         self.assertTrue(marriageBeforeDivorce(families["@F2@"])) # no divorce date (Stuart & Agnes)
         self.assertTrue(marriageBeforeDivorce(families["@F3@"])) # no divorce date for same husband (Stuart & Edith)
 
-    def story5(self):
+    def testStory5(self):
         '''Marriage before death'''
         self.assertFalse(marriageBeforeDeath(individuals["@I8@"])) # marriage date after death date (Edith)
         self.assertTrue(marriageBeforeDeath(individuals["@I6@"])) # marriage date same as death date (Margo)
