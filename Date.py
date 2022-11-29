88414cbfd48f02dd31e2a53a29c22f7a74a664b8
ScienceWater <vtufo@sbcglobal.net>
2022-11-28 20:11:50 -0500

    Corrected missing parentheses.

--- Date.py
+++ Date.py
@@ -100,4 +100,4 @@ class Date():
     def exists(self):
         '''Returns true iff self represents a valid date. A date can be invalid because
            the month does not exist or because the day does not exist within the given month/year.'''
-        return self.month in MONTHS and 1 <= self.day <= self.__daysInMonth
+        return self.month in MONTHS and 1 <= self.day <= self.__daysInMonth()
